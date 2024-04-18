e2ed0382db75c82969367e5067b5c95068ef49b0
immunnapandit <munnapandit2002@gmail.com>
2024-04-18 18:27:15 +0530

    add social media icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { FaFacebook, FaTwitter, FaInstagram, FaSnapchat, FaYoutube, FaMedium } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -7,15 +8,26 @@ const Footer = () => {
             <div className="hidden md:flex items-center">
                 <ul className="flex">
                     <li className="px-4">
-                        <Link to="/privacy" className="hover:text-blue-900">Privacy Policy</Link>
+                        <a href="/privacy" className="hover:text-blue-900">Privacy Policy</a>
                     </li>
                     <li className="px-4">
-                        <Link to="/cookiepolicy" className="hover:text-blue-900">Cookie Policy</Link>
+                        <a href="/cookiepolicy" className="hover:text-blue-900">Cookie Policy</a>
                     </li>
                     <li className="px-4">
-                        <Link to="/contactus" className="hover:text-blue-900">Contact Us</Link>
+                        <a href="/contactus" className="hover:text-blue-900">Contact Us</a>
                     </li>
                 </ul>
+            </div>
+            <div className="flex items-center justify-center">
+                <span className="text-gray-600 mr-4">Follow us:</span>
+                <IconContext.Provider value={{ size: "1.5em", className: "mr-4" }}>
+                    <a href="https://www.facebook.com/immunnapandit/" target="_blank" rel="noopener noreferrer"><FaFacebook style={{ color: "#1877f2" }} /></a>
+                    <a href="https://twitter.com/immunnapandit/" target="_blank" rel="noopener noreferrer"><FaTwitter style={{ color: "#1da1f2" }} /></a>
+                    <a href="https://www.instagram.com/immunnapandit/" target="_blank" rel="noopener noreferrer"><FaInstagram style={{ color: "#c13584" }} /></a>
+                    <a href="https://www.snapchat.com/immunnapandit/" target="_blank" rel="noopener noreferrer"><FaSnapchat style={{ color: "#fffc00" }} /></a>
+                    <a href="https://www.youtube.com/@technicalmunnapandit" target="_blank" rel="noopener noreferrer"><FaYoutube style={{ color: "#ff0000" }} /></a>
+                    <a href="https://medium.com/immunnapandit" target="_blank" rel="noopener noreferrer"><FaMedium style={{ color: "#12100e" }} /></a>
+                </IconContext.Provider>
             </div>
         </footer>
     );
