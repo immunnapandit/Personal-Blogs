ea2768912ef5d4939b0df889b05ffaf6e2818369
immunnapandit <munnapandit2002@gmail.com>
2024-04-18 13:34:48 +0530

    memories photo added

--- src/Components/Memories.js
+++ src/Components/Memories.js
@@ -1,17 +1,34 @@
 import React from "react";
 import munnaImage from "../assets/munna.jpg";
+import mumbaiImage from "../assets/munna1.jpeg";
+import officeImage from "../assets/munna2.jpeg";
+import resImage from "../assets/munna3.jpeg";
+import collegeImages from "../assets/munna4.jpeg";
+import stationImages from "../assets/munna6.jpeg";
 
 const Memories = () => {
     return (
-        <div className="h-screen flex flex-col overflow-x-hidden overflow-y-auto">
+        <div className="min-h-screen flex flex-col overflow-x-hidden overflow-y-auto">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
                 <h1 className="text-3xl md:text-5xl text-center mb-20">My Memories</h1>
                 <div className="flex flex-wrap justify-between">
-                    <figure className="w-full md:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md">
-                    <img src={munnaImage} alt="memoriesphoto" className="w-full h-full object-cover filter drop-shadow-lg" />
-                        <figcaption className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-black to-transparent text-white px-6 py-4 text-base font-semibold opacity-0 transition-opacity duration-300">
-                            Munna
-                        </figcaption>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={munnaImage} alt="memoriesphoto" className="w-full h-full object-cover filter drop-shadow-lg" />
+                    </figure>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={mumbaiImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg" />
+                    </figure>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={officeImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg" />
+                    </figure>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={resImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg" />
+                    </figure>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={collegeImages} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg" />
+                    </figure>
+                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
+                        <img src={stationImages} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg" />
                     </figure>
                 </div>
             </div>
