06272970642a21d9ffcfc7ad3d6fa57776dba91d
immunnapandit <munnapandit2002@gmail.com>
2024-04-26 01:13:35 +0530

    next and prvev button features

--- src/Components/Memories.js
+++ src/Components/Memories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import munnaImage from "../assets/munna.jpg";
 import mumbaiImage from "../assets/munna1.jpeg";
 import officeImage from "../assets/munna2.jpeg";
@@ -8,67 +8,79 @@ import friendsImage from "../assets/munna5.jpeg";
 import stationImages from "../assets/munna6.jpeg";
 import ColllegeImages from "../assets/munna9.jpeg";
 import gauravfriends from "../assets/munna7.jpeg";
-import rajabhoj from "../assets/rajabhoj.jpeg"
-import rajabhoj1 from "../assets/rajabhoj1.jpeg"
-import mahakal from "../assets/mahakal.jpeg"
-import juhubeach from "../assets/juhubeach.jpeg"
-import juhubeach1 from "../assets/juhubeach1.jpeg"
+import rajabhoj from "../assets/rajabhoj.jpeg";
+import rajabhoj1 from "../assets/rajabhoj1.jpeg";
+import mahakal from "../assets/mahakal.jpeg";
+import juhubeach from "../assets/juhubeach.jpeg";
+import juhubeach1 from "../assets/juhubeach1.jpeg";
 import sky from "../assets/sky.jpeg";
+import swimming from "../assets/swimming.jpeg";
 
+const images=[
+    munnaImage,
+    mumbaiImage,
+    officeImage,
+    resImage,
+    collegeImages,
+    stationImages,
+    sky,
+    juhubeach,
+    juhubeach1,
+    ColllegeImages,
+    gauravfriends,
+    rajabhoj,
+    rajabhoj1,
+    mahakal,
+    friendsImage,
+    swimming
+];
 
 const Memories = () => {
+
+    const[selectedImageIndex, setSelectedImageIndex] = useState(null);
+    const[modalOpen, setModalOpen] = useState(false);
+
+    const openModal = (index) => {
+        setSelectedImageIndex(index);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedImageIndex(null);
+        setModalOpen(false);
+    }
+
+    const nextImage = () =>{
+        setSelectedImageIndex((prevIndex) => (prevIndex === images.length -1 ? 0 : prevIndex +1));
+    }
+
+    const prevImages = () => {
+        setSelectedImageIndex((prevIndex) => (prevIndex === 0 ? images.length -1 : prevIndex -1));
+    }
+
     return (
         <div className="min-h-screen flex flex-col overflow-x-hidden overflow-y-auto">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
                 <h1 className="text-3xl md:text-5xl text-center mb-20">My Memories</h1>
                 <div className="flex flex-wrap justify-between">
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={munnaImage} alt="memoriesphoto" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={mumbaiImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={officeImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={resImage} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={collegeImages} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={stationImages} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={friendsImage} alt="memories" className=""></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={ColllegeImages} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={gauravfriends} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={rajabhoj} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={rajabhoj1} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={mahakal} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={juhubeach} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={juhubeach1} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
-                    <figure className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4">
-                        <img src={sky} alt="memories" className="h-full w-full object-cover"></img>
-                    </figure>
+                    {images.map((image,index) =>(
+                        <figure key={index} className="w-full md:w-1/2 lg:w-1/3 card mb-8 md:mb-0 transform transition-transform hover:scale-103 hover:shadow-md px-4"
+                        onClick={()=> openModal(index)}>
+                            <img src={image} alt="memories" className="w-full h-full object-cover filter drop-shadow-lg py-3" />
+                        </figure>
+                    ))}
                 </div>
             </div>
+            {modalOpen &&(
+                 <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50">
+                    <div className="max-w-xl w-full relative">
+                        <button onClick={closeModal} className="absolute top-0 right-0 m-4 text-white text-2xl">&#10006;</button>
+                        <button onClick={prevImages} className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white text-2xl font-bold" style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: '8px 16px', borderRadius: '4px' }}>&#8249;</button>
+                        <button onClick={nextImage} className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white text-2xl font-bold" style={{ backgroundColor: 'rgba(0,0,0,0.5)', padding: '8px 16px', borderRadius: '4px' }}>&#8250;</button>
+                        <img src={images[selectedImageIndex]} alt="memories" className="w-full h-auto" />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
