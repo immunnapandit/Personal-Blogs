7b0fe8bb0a36560aa6f7a145b47b1ec796ffee29
immunnapandit <munnapandit2002@gmail.com>
2024-05-02 17:19:15 +0530

    features add in about

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,21 +2,9 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-4">Welcome to TechWithMunna!</h1>
-        <p className="text-lg text-gray-700 mb-8">
-          I'm Munna Kumar Pandit, a passionate software engineer dedicated to exploring the vast realms of technology and sharing my knowledge with you.
-        </p>
-        <p className="text-lg text-gray-700 mb-8">
-          I graduated with a degree in Computer Science from IES College of Technology, Bhopal, and since then, I've been on an exciting journey of continuous learning and growth. My curiosity and eagerness to learn have driven me to achieve strong academic records and develop a deep understanding of programming languages like C/C++ and Python.
-        </p>
-        <p className="text-lg text-gray-700 mb-8">
-          As a software engineer, I am committed to showcasing my technical skills and expertise in every project I undertake. I strive to contribute to the tech community by providing valuable insights, tutorials, and resources through TechWithMunna.
-        </p>
-        <p className="text-lg text-gray-700">
-          Join me on this enriching journey, and let's explore the fascinating world of technology together! Thank you for being a part of TechWithMunna.
-        </p>
+    <div>
+      <div>
+        <h1>About me</h1>
       </div>
     </div>
   );
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,19 +1,25 @@
 import React from "react";
 import munnaImage from "../assets/munna.jpg";
-import "./Body";
+// import "./Body.css";
+import About from "./About";
 
 const Body = () => {
     return (
-        <div className="flex flex-col md:flex-row items-center py-18 m-20">
-            <div className="max-w-sm md:mr-10 mb-10 md:mb-0">
-                <img src={munnaImage} alt="munnaimage" className="w-full rounded-full" />
+        <div className="flex flex-col">
+            <div className="flex flex-col md:flex-row items-center py-18 m-20">
+                <div className="max-w-sm md:mr-10 mb-10 md:mb-0">
+                    <img src={munnaImage} alt="munnaimage" className="w-full rounded-full" />
+                </div>
+                <div className="text-center md:text-left">
+                    <h1 className="nij-manrope-bold text-3xl md:text-5xl mb-4 md:mb-6">
+                        Welcome to <span style={{ color: '#ff0000' }}>Tech</span>with<span style={{ color: '#3182ce' }}>munna</span><br />
+                        <span className="text-blue-500">I'm</span> Munna, a passionate<br />
+                        <span className="typing">Software Engineer</span>
+                    </h1>
+                </div>
             </div>
-            <div className="text-center md:text-left">
-                <h1 className="nij-manrope-bold text-3xl md:text-5xl mb-4 md:mb-6">
-                Welcome to <span style={{ color: '#ff0000' }}>Tech</span>with<span style={{ color: '#3182ce'}}>munna</span><br />
-                <span className="text-blue-500">I'm</span> Munna, a passionate<br/>
-                <span className="typing">Software Engineer</span>
-                </h1>
+            <div className="flex justify-center">
+                <About/>
             </div>
         </div>
     );
