258752dcc569e61b4b05e5a2c04ea850bc94b7f8
immunnapandit <munnapandit2002@gmail.com>
2024-05-06 00:15:11 +0530

    dfs

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 const port = 3001; // Use any port you prefer
