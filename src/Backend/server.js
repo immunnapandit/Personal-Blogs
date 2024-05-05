644f083f5097a24a7ae6e7055faf20e9fc05cf8c
immunnapandit <munnapandit2002@gmail.com>
2024-05-06 00:06:02 +0530

    kk

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -4,45 +4,45 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3001; // Use any port you prefer
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'P@#andit@#001',
-  database: 'my_database'
+    host: 'localhost',
+    user: 'root',
+    password: 'P@#andit@#001',
+    database: 'my_database'
 });
 
 db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to database');
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
 });
 
-// Use the cors middleware with specific options for production
-app.use(cors({
-  origin: 'https://techwithmunna.vercel.app', // Allow requests from this specific origin only
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 
 app.use(bodyParser.json());
 
 app.post('/submit-form', (req, res) => {
-  const { name, email, subject, message } = req.body;
-  const sql = 'INSERT INTO contact_form (name, email, subject, message) VALUES (?, ?, ?, ?)';
-  db.query(sql, [name, email, subject, message], (err, result) => {
-    if (err) {
-      console.error('Error inserting into database:', err);
-      res.status(500).json({ error: 'An error occurred while saving data' });
-      return;
-    }
-    console.log('Data inserted into database');
-    res.json({ message: 'Form submitted successfully!' });
-  });
+    const { name, email, subject, message } = req.body;
+    const sql = 'INSERT INTO contact_form (name, email, subject, message) VALUES (?, ?, ?, ?)';
+    db.query(sql, [name, email, subject, message], (err, result) => {
+        if (err) {
+            console.error('Error inserting into database:', err);
+            res.status(500).json({ error: 'An error occurred while saving data' });
+            return;
+        }
+        console.log('Data inserted into database');
+        res.json({ message: 'Form submitted successfully!' });
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
