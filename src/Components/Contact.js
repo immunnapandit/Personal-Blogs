c7cafa0dc2058738a49ce3c5827bd7517d8053c3
immunnapandit <munnapandit2002@gmail.com>
2024-05-05 22:13:24 +0530

    backend code

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,6 +8,7 @@ const Contact = () => {
         subject: "",
         message: ""
     });
+    const [submitStatus, setSubmitStatus] = useState(null);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -18,29 +19,30 @@ const Contact = () => {
     };
 
     const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          const response = await fetch('http://localhost:3001/submit-form', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(formData),
-          });
-          const data = await response.json();
-          console.log(data); // Log the response from the backend
-          // Reset the form after successful submission
-          setFormData({
-              name: "",
-              email: "",
-              subject: "",
-              message: ""
-          });
-      } catch (error) {
-          console.error('Error:', error);
-      }
-  };
-  
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3001/submit-form', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            console.log(data);
+            setSubmitStatus('success');
+            // Reset the form after successful submission
+            setFormData({
+                name: "",
+                email: "",
+                subject: "",
+                message: ""
+            });
+        } catch (error) {
+            console.error('Error:', error);
+            setSubmitStatus('error');
+        }
+    };
 
     return (
         <div className="max-w-screen-xl mx-auto py-20 flex flex-col md:flex-row items-center">
@@ -49,6 +51,8 @@ const Contact = () => {
             </div>
             <div className="md:w-1/2 p-8">
                 <h1 className="text-2xl font-bold mb-4">GET IN TOUCH</h1>
+                {submitStatus === 'success' && <p className="text-green-500">Message sent successfully!</p>}
+                {submitStatus === 'error' && <p className="text-red-500">Error sending message. Please try again later.</p>}
                 <form onSubmit={handleSubmit} className="w-full">
                     <div className="mb-4">
                         <label htmlFor="name" className="block font-bold mb-2">Name</label>
