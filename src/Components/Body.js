4d23bfd9209c000f2f084c2432cbf039c4a3b877
MUNNA KUMAR <munnapandit2002@gmail.com>
2024-07-18 20:25:31 +0530

    Add features in Body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BodyImage from "../assets/bodyimage1.jpg";
-import OverlayImage from "../assets/munna.jpg"; // Import the new image
+// import OverlayImage from "../assets/munna.jpg"; // Import the new image
 import Services from "./Services";
 import Community from "./Community";
 import About from "./About";
@@ -18,7 +18,7 @@ const Body = () => {
                         pursuing
                     </span>
                 </div>
-                <img alt="Overlay" src={OverlayImage} className="absolute bottom-0 right-0 w-20 h-20 md:w-28 md:h-28 lg:w-36 lg:h-36 xl:w-48 xl:h-48 object-cover m-4" />
+                {/* <img alt="Overlay" src={OverlayImage} className="absolute bottom-0 right-0 w-20 h-20 md:w-28 md:h-28 lg:w-36 lg:h-36 xl:w-48 xl:h-48 object-cover m-4" /> */}
             </div>
             <div className="flex justify-center px-4 md:px-8 lg:px-16 xl:px-32">
                 <About/>
